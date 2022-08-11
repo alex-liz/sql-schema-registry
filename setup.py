6c728b05cf6293714fb3ac67884ffe1cb64455a3
alex <alejandrolizaguilera@gmail.com>
2022-08-11 18:13:32 +0100

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='sql_schema_registry',
+    packages=find_packages(include=['sql_schema_registry']),
+    version='0.1.0',
+    description='Schema registry',
+    author='Alejandro Liz',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
